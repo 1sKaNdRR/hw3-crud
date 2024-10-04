350205b503a053ea4002aca45ad3f6db8900a3d2
1sKaNdRR <151454522+1sKaNdRR@users.noreply.github.com>
2024-10-04 12:40:40 +0200

    hw-5

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -2,72 +2,77 @@ import createHttpError from 'http-errors';
 import parsePaginationParams from '../utils/parsePaginationParams.js';
 import parseSortParams from '../utils/parseSortParams.js';
 import * as contactServices from '../services/contacts.js';
-import {parseContactsFilterParams} from '../utils/filters/parseContactsFilterParams.js';
+import { parseContactsFilterParams } from '../utils/filters/parseContactsFilterParams.js';
 import { sortFields } from '../db/Contacts.js';
 
 export const getAllContactsController = async (req, res) => {
   const { perPage, page } = parsePaginationParams(req.query);
   const { sortBy, sortOrder } = parseSortParams({ ...req.query, sortFields });
   const filter = parseContactsFilterParams(req.query);
-  const {_id: userId} = req.user;
-    const data = await contactServices.getContacts({
-      perPage,
+  const { _id: userId } = req.user;
+
+  const data = await contactServices.getContacts({
+    perPage,
     page,
     sortBy,
     sortOrder,
-    filter: {...filter, userId},
-    });
+    filter: { ...filter, userId },
+  });
 
-    res.json({
-      status: 200,
-      message: 'Successfully found contacts',
-      data,
-    });
+  res.json({
+    status: 200,
+    message: 'Successfully found contacts',
+    data,
+  });
 };
+
 
 export const getContactByIdController = async (req, res) => {
   const { id } = req.params;
-  const {_id: userId} = req.user;
-  const data = await contactServices.getContact({_id: id, userId});
+  const { _id: userId } = req.user;
+
+
+  const data = await contactServices.getContact({ _id: id, userId });
 
   if (!data) {
-    throw createHttpError(404, `Contact with id=${id} not found`);
+    throw createHttpError(404, `Contact with id=${id} not found or does not belong to user`);
   }
 
   res.json({
     status: 200,
-    message: `Contact with ${id} successfully find`,
+    message: `Contact with id=${id} successfully found`,
     data,
   });
 };
 
-export const addContactController = async(req, res)=> {
-  const {_id: userId} = req.user;
-  const data = await contactServices.createContact({...req.body, userId});
-  
+
+export const addContactController = async (req, res) => {
+  const { _id: userId } = req.user;
+  const data = await contactServices.createContact({ ...req.body, userId });
+
   res.status(201).json({
     status: 201,
-    message: "Contact add successfully",
+    message: 'Contact added successfully',
     data,
   });
 };
 
-export const upsertContactController = async(req, res)=> {
-  const {id} = req.params;
-  const {isNew, data} = await contactServices.updateContact({_id: id}, req.body, {upsert: true});
+export const upsertContactController = async (req, res) => {
+  const { id } = req.params;
+  const { isNew, data } = await contactServices.updateContact({ _id: id }, req.body, { upsert: true });
 
   const status = isNew ? 201 : 200;
 
   res.status(status).json({
     status,
-    message: "Contact upsert successfully",
+    message: 'Contact upserted successfully',
     data,
   });
 };
 
-export const patchContactController = async(req, res)=> {
-  const {id} = req.params;
-  const result = await contactServices.updateContact({_id: id}, req.body);
+export const patchContactController = async (req, res) => {
+  const { id } = req.params;
+  const result = await contactServices.updateContact({ _id: id }, req.body);
 
   if (!result) {
     throw createHttpError(404, `Contact with id=${id} not found`);
@@ -75,14 +80,14 @@ export const patchContactController = async(req, res)=> {
 
   res.json({
     status: 200,
-    message: "Contact patched successfully",
+    message: 'Contact patched successfully',
     data: result.data,
   });
 };
 
-export const deleteContactController = async(req, res)=> {
-  const {id} = req.params;
-  const data = await contactServices.deleteContact({_id: id});
+export const deleteContactController = async (req, res) => {
+  const { id } = req.params;
+  const data = await contactServices.deleteContact({ _id: id });
 
   if (!data) {
     throw createHttpError(404, `Contact with id=${id} not found`);
@@ -90,3 +95,103 @@ export const deleteContactController = async(req, res)=> {
 
   res.status(204).send();
 };
+
+
+
+
+
+
+
+
+// import createHttpError from 'http-errors';
+// import parsePaginationParams from '../utils/parsePaginationParams.js';
+// import parseSortParams from '../utils/parseSortParams.js';
+// import * as contactServices from '../services/contacts.js';
+// import {parseContactsFilterParams} from '../utils/filters/parseContactsFilterParams.js';
+// import { sortFields } from '../db/Contacts.js';
+
+// export const getAllContactsController = async (req, res) => {
+//   const { perPage, page } = parsePaginationParams(req.query);
+//   const { sortBy, sortOrder } = parseSortParams({ ...req.query, sortFields });
+//   const filter = parseContactsFilterParams(req.query);
+//   const {_id: userId} = req.user;
+//     const data = await contactServices.getContacts({
+//       perPage,
+//     page,
+//     sortBy,
+//     sortOrder,
+//     filter: {...filter, userId},
+//     });
+
+//     res.json({
+//       status: 200,
+//       message: 'Successfully found contacts',
+//       data,
+//     });
+// };
+
+// export const getContactByIdController = async (req, res) => {
+//   const { id } = req.params;
+//   const {_id: userId} = req.user;
+//   const data = await contactServices.getContact({_id: id, userId});
+
+//   if (!data) {
+//     throw createHttpError(404, `Contact with id=${id} not found`);
+//   }
+
+//   res.json({
+//     status: 200,
+//     message: `Contact with ${id} successfully find`,
+//     data,
+//   });
+// };
+
+// export const addContactController = async(req, res)=> {
+//   const {_id: userId} = req.user;
+//   const data = await contactServices.createContact({...req.body, userId});
+  
+//   res.status(201).json({
+//     status: 201,
+//     message: "Contact add successfully",
+//     data,
+//   });
+// };
+
+// export const upsertContactController = async(req, res)=> {
+//   const {id} = req.params;
+//   const {isNew, data} = await contactServices.updateContact({_id: id}, req.body, {upsert: true});
+
+//   const status = isNew ? 201 : 200;
+
+//   res.status(status).json({
+//     status,
+//     message: "Contact upsert successfully",
+//     data,
+//   });
+// };
+
+// export const patchContactController = async(req, res)=> {
+//   const {id} = req.params;
+//   const result = await contactServices.updateContact({_id: id}, req.body);
+
+//   if (!result) {
+//     throw createHttpError(404, `Contact with id=${id} not found`);
+//   }
+
+//   res.json({
+//     status: 200,
+//     message: "Contact patched successfully",
+//     data: result.data,
+//   });
+// };
+
+// export const deleteContactController = async(req, res)=> {
+//   const {id} = req.params;
+//   const data = await contactServices.deleteContact({_id: id});
+
+//   if (!data) {
+//     throw createHttpError(404, `Contact with id=${id} not found`);
+//   }
+
+//   res.status(204).send();
+// };
--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,5 +1,4 @@
 import ContactCollection from "../db/Contacts.js";
-
 import calculatePaginationData from '../utils/calculatePaginationData.js';
 import { SORT_ORDER } from '../constants/index.js';
 
@@ -9,25 +8,24 @@ export const getContacts = async ({
     sortBy = "_id", 
     sortOrder = SORT_ORDER[0],
     filter = {},
-})=> {
+}) => {
     const skip = (page - 1) * perPage;
-    const movieQuery = ContactCollection.find(); 
-    
-    if(filter.minReleaseYear) {
+    const movieQuery = ContactCollection.find();
+
+    if (filter.minReleaseYear) {
         movieQuery.where("releaseYear").gte(filter.minReleaseYear);
     }
-    if(filter.maxReleaseYear) {
+    if (filter.maxReleaseYear) {
         movieQuery.where("releaseYear").lte(filter.maxReleaseYear);
     }
-    if(filter.userId) {
+    if (filter.userId) {
         movieQuery.where("userId").eq(filter.userId);
     }
 
-    const movies = await movieQuery.skip(skip).limit(perPage).sort({[sortBy]: sortOrder});
-    
+    const movies = await movieQuery.skip(skip).limit(perPage).sort({ [sortBy]: sortOrder });
     const count = await ContactCollection.find().merge(movieQuery).countDocuments();
 
-    const paginationData = calculatePaginationData({count, perPage, page});
+    const paginationData = calculatePaginationData({ count, perPage, page });
 
     return {
         page,
@@ -38,17 +36,20 @@ export const getContacts = async ({
     };
 };
 
-export const getContact = filter => ContactCollection.findById(filter);
 
-export const createContact = payload => ContactCollection.create(payload);
+export const getContact = (filter) => ContactCollection.findOne(filter);
 
-export const updateContact = async(filter, data, options = {})=> {
+
+export const createContact = (payload) => ContactCollection.create(payload);
+
+
+export const updateContact = async (filter, data, options = {}) => {
     const rawResult = await ContactCollection.findOneAndUpdate(filter, data, {
         includeResultMetadata: true,
         ...options,
     });
 
-    if(!rawResult || !rawResult.value) return null;
+    if (!rawResult || !rawResult.value) return null;
 
     return {
         data: rawResult.value,
@@ -56,4 +57,82 @@ export const updateContact = async(filter, data, options = {})=> {
     };
 };
 
-export const deleteContact = filter => ContactCollection.findOneAndDelete(filter);
+export const deleteContact = (filter) => ContactCollection.findOneAndDelete(filter);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import ContactCollection from "../db/Contacts.js";
+
+// import calculatePaginationData from '../utils/calculatePaginationData.js';
+// import { SORT_ORDER } from '../constants/index.js';
+
+// export const getContacts = async ({
+//     perPage,
+//     page, 
+//     sortBy = "_id", 
+//     sortOrder = SORT_ORDER[0],
+//     filter = {},
+// })=> {
+//     const skip = (page - 1) * perPage;
+//     const movieQuery = ContactCollection.find(); 
+    
+//     if(filter.minReleaseYear) {
+//         movieQuery.where("releaseYear").gte(filter.minReleaseYear);
+//     }
+//     if(filter.maxReleaseYear) {
+//         movieQuery.where("releaseYear").lte(filter.maxReleaseYear);
+//     }
+//     if(filter.userId) {
+//         movieQuery.where("userId").eq(filter.userId);
+//     }
+
+//     const movies = await movieQuery.skip(skip).limit(perPage).sort({[sortBy]: sortOrder});
+    
+//     const count = await ContactCollection.find().merge(movieQuery).countDocuments();
+
+//     const paginationData = calculatePaginationData({count, perPage, page});
+
+//     return {
+//         page,
+//         perPage,
+//         movies,
+//         totalItems: count,
+//         ...paginationData,
+//     };
+// };
+
+// export const getContact = filter => ContactCollection.findById(filter);
+
+// export const createContact = payload => ContactCollection.create(payload);
+
+// export const updateContact = async(filter, data, options = {})=> {
+//     const rawResult = await ContactCollection.findOneAndUpdate(filter, data, {
+//         includeResultMetadata: true,
+//         ...options,
+//     });
+
+//     if(!rawResult || !rawResult.value) return null;
+
+//     return {
+//         data: rawResult.value,
+//         isNew: Boolean(rawResult?.lastErrorObject?.upserted),
+//     };
+// };
+
+// export const deleteContact = filter => ContactCollection.findOneAndDelete(filter);
